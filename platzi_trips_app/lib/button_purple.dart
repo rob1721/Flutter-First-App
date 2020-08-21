c43b851bf1c394f0ea45d4d9fefe5d564d495ee7
Roberto Valenzuela <robval.1721@gmail.com>
2020-08-21 01:34:15 -0400

    feat(lib): implemented button with InkWell, pay attention to doc

---
+++ platzi_trips_app/lib/button_purple.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+//no es el boton en si, si no..es el inkwell
+//es el reconocer un sector como toucheable
+// ignore: must_be_immutable
+class ButtonPurple extends StatelessWidget {
+
+  String buttonText = "Navigate";
+
+  //constructor
+  ButtonPurple(this.buttonText);
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      //dando propiedades, en este caso, 
+      //reaccionando al tap
+      onTap: () {
+        Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Navegando..."),
+          )
+        );
+      },
+      //daremos la forma que queremos q tenga el
+      //boton
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 30.0,
+          left: 20.0,
+          right: 20.0,
+
+        ),
+        height: 50.0,
+        width: 180.0,
+        decoration: BoxDecoration(
+          //borde circular
+          borderRadius: BorderRadius.circular(30.0),
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF4268D3),
+            //Color(0XFF584CD1), color original medio morado
+              Color(0X00A321), 
+              //puedes agregar + de 2 colores
+            ],
+            begin: FractionalOffset(0.2, 0.4),
+            end: FractionalOffset(0.4, 3.5),
+            stops: [0.0, 0.6],
+            tileMode: TileMode.clamp,
+          )
+        ),
+        //centrando el texto
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontFamily: "Lato",
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
