a75aa7c75202adf2b7efa741e308fd3d57269377
Roberto Valenzuela <robval.1721@gmail.com>
2020-08-25 00:26:31 -0400

    feat(lib): added file to get all the things to be calles on main

---
+++ platzi_trips_app/lib/platzi_trips.dart
@@ -0,0 +1,44 @@
+//aca vamos a contener toda la logica, toda la app
+//la cual sera llamada en el main en lugar del body/scaffold
+import 'package:flutter/material.dart';
+
+class PlatziTrips extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _PlatziTrips();
+  }
+  
+}
+
+class _PlatziTrips extends State<PlatziTrips>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //bottonNavigationBar casual "normalito"
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.white,
+            primaryColor: Colors.purple
+          ), //la diferencia q queremos
+          child: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text(""),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text(""),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text(""),
+              )
+            ]//los tabs del buttonnavigation
+
+          )
+        ),
+      );
+  }
+  
+}
