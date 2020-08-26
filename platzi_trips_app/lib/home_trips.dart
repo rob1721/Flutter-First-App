24b2380093fcc9dd816c4fe150b1687a294e9276
Roberto Valenzuela <robval.1721@gmail.com>
2020-08-26 01:53:49 -0400

    chore(lib): comment just to get no alert

--- platzi_trips_app/lib/home_trips.dart
+++ platzi_trips_app/lib/home_trips.dart
@@ -4,13 +4,13 @@ import 'description_place.dart';
 import 'header_appbar.dart';
 import 'review_list.dart';
 
+// ignore: must_be_immutable
 class HomeTrips extends StatelessWidget {
 
   String descriptionDummy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.\n\nQuis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Stack( // pon uno encima de otro
           children: <Widget>[
             ListView(
