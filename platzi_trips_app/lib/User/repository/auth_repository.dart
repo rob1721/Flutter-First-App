5290729a987f5663182eba968478570bb2b3cd5d
Roberto Valenzuela <robval.1721@gmail.com>
2020-09-01 15:25:18 -0400

    chore(repository): "this is like module but for dart"

--- platzi_trips_app/lib/User/repository/auth_repository.dart
+++ platzi_trips_app/lib/User/repository/auth_repository.dart
@@ -1,7 +1,7 @@
 // este es el repositorio q contiene la logica de repository
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:platzi_trips_app/User/repository/firebase_auth_api.dart';
-
+// este es como el module
 class AuthRepository {
   final _firebaseAuthAPI = FirebaseAuthAPI();
 
