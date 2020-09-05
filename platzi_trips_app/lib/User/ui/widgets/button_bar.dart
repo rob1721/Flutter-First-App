b06a82e85ed38e4db1cabab842f8a3cc95a031f8
Roberto Valenzuela <robval.1721@gmail.com>
2020-09-04 20:31:38 -0400

    implementando sign out de lleno, falta la puesta a botones

--- platzi_trips_app/lib/User/ui/widgets/button_bar.dart
+++ platzi_trips_app/lib/User/ui/widgets/button_bar.dart
@@ -1,10 +1,17 @@
+// ahora mas genéricos
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'circle_button.dart';
 
+// ignore: must_be_immutable
 class ButtonsBar extends StatelessWidget {
+
+  UserBloc userBloc;
 
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
     return Padding(
         padding: EdgeInsets.symmetric(
             horizontal: 0.0,
@@ -12,11 +19,20 @@ class ButtonsBar extends StatelessWidget {
         ),
         child: Row(
           children: <Widget>[
-            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.card_travel, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.mail_outline, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
-            CircleButton(true, Icons.person, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+
+            // cambiar la contraseña
+            
+            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+            // ojo q el valor key q podría haber necesitado en el '()'
+            () => {}),
+            // añadiendo un nuevo lugar
+            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1),
+            () => {}),
+            // cerrar sesion
+            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 1),
+            () => {
+              userBloc.signOut(),
+            }),
           ],
         )
     );
