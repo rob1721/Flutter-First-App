a8910b9804195fc4166a38dac9fb58b9e3d14f83
Roberto Valenzuela <robval.1721@gmail.com>
2020-09-06 22:57:37 -0300

    arreglado recepcion de imagen desde caamara

--- platzi_trips_app/lib/User/ui/widgets/button_bar.dart
+++ platzi_trips_app/lib/User/ui/widgets/button_bar.dart
@@ -6,7 +6,6 @@ import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:platzi_trips_app/Place/ui/screens/add_place_screen.dart';
 import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
-import 'package:platzi_trips_app/User/ui/widgets/camera.dart';
 import 'circle_button.dart';
 
 // ignore: must_be_immutable
@@ -32,35 +31,15 @@ class ButtonsBar extends StatelessWidget {
             () => {}),
             // añadiendo un nuevo lugar
             CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1),
-              () /*async*/ {
-                /*var _image = await ImagePicker().getImage(source: ImageSource.camera);
-
-                //File lilo = File.fromRawPath(await _image.readAsBytes());
-
-                File asd = File(_image.path);
-                File asd2 = File.fromRawPath(await _image.readAsBytes());
-
-                // casteando de PickedFile a File
-                //_image: DecorationImage(image: FileImage(File(_image.path)));
-                // OBTENIENDO LA IMAGEN
-                //ImagePicker imagePicker;
-                /*CUAL ES LA FUENTE Q QUEREMOS MOSTRAR */
-                if (_image != null){
-                  Navigator.push(context,
-                    MaterialPageRoute(builder:
-                      (BuildContext context) => AddPlaceScreen(image: _image),
-                    ),
-                  ); //push
-                } //if*/
-                // ignore: deprecated_member_use
-                ImagePicker.pickImage(source: ImageSource.camera)
-                    .then((File image) {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (BuildContext context) => AddPlaceScreen(image: image)));
-                }).catchError((onError) => print(onError));
-
-                  
-                    
+              () async {
+                final ImagePicker picker = ImagePicker();
+                final PickedFile pickedFile = await picker.getImage(source: ImageSource.camera);
+                final File image = File(pickedFile.path);
+                await Navigator.push( context, 
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => AddPlaceScreen(image: image)
+                  )
+                );
               } //async
                 // AÑADIENDO LUGAR PANTALLA
               
